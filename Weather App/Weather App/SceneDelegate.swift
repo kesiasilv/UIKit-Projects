f0809c6f916a0a777ed2dd1e63fda8d6d6fe8d31
Késia Silva <kesiasilvab6@gmail.com>
2026-05-14 17:54:48 -0300

    feat: quadrado em storyboard finalizado

--- Weather App/Weather App/SceneDelegate.swift
+++ Weather App/Weather App/SceneDelegate.swift
@@ -18,10 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
+        window = UIWindow(windowScene: windowScene)
+//        window?.windowScene = windowScene
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
+        
+ 
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -8,19 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //metodo didLoad -> metodo de ciclo de vida que é executada sempre que a viewController é carregada
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        setupView()
+//        view.backgroundColor = .red
+//        
+//                //criando um quadrado
+//                let customView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+//                customView.backgroundColor = .blue
+//                //adicionando o quadrado na tela
+//                view.addSubview(customView)
+        
     }
     
-    //esse metodo é executado toda vez que a tela aparece, quando ela é vizivel 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    //    //esse metodo é executado toda vez que a tela aparece, quando ela é vizivel
+    //    override func viewDidAppear(_ animated: Bool) {
+    //        super.viewDidAppear(animated)
+    //
+    //        performSegue(withIdentifier: "toForecast", sender: nil)
+    //    }
+    
+    private func setupView() {
+//        view.backgroundColor = .red
         
-        performSegue(withIdentifier: "toForecast", sender: nil)
+        let customView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        customView.backgroundColor = .red
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(customView)
+        
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+            //            customView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            //            customView.widthAnchor.constraint(equalToConstant: 100),
+            //            customView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
     }
 }
 

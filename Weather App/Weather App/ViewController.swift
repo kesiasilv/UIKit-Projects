ec302c9e4bd82595026e4ddb1a4a992814769bca
Késia Silva <kesiasilvab6@gmail.com>
2026-05-14 10:38:16 -0300

    feat: estudando sobre metodos de apresetacao no viewcode

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //metodo didLoad -> metodo de ciclo de vida que é executada sempre que a viewController é carregada
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
+    
+    //esse metodo é executado toda vez que a tela aparece, quando ela é vizivel 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        performSegue(withIdentifier: "toForecast", sender: nil)
+    }
 }
 
